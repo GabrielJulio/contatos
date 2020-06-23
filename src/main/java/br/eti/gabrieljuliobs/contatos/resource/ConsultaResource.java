742984c4f261913efc90b38994fe158b8facd35a
Gabriel Júlio <gabrieljuliobs@gmail.com>
2020-06-23 20:09:20 -0300

    Criado pacote Resource

---
+++ src/main/java/br/eti/gabrieljuliobs/contatos/resource/ConsultaResource.java
@@ -0,0 +1,10 @@
+package br.eti.gabrieljuliobs.contatos.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/contatos")
+public class ConsultaResource {
+
+}
